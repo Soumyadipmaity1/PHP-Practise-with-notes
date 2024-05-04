a7e6b58e3c41833168106211fb94a3e219034574
smilingsoumya1 <soumyadipmaityjee1@gmail.com>
2024-05-04 21:50:15 +0530

    variables in php

---
+++ 01_Basics/Variables.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML With PHP</title>
+</head>
+<body>
+    <h1> Hello World</h1>
+    <?php
+    $a=5 ; $b=9 ;
+    echo $a + $b;
+    // echo "I am Soumyadip";
+    ?>
+</body>
+</html>
