f92f8089a60e867fb7a51e461467af878d2771bb
smilingsoumya1 <soumyadipmaityjee1@gmail.com>
2024-05-05 23:58:51 +0530

    post method updation

--- post_method.php
+++ post_method.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Get Method</title>
+    <title>Post Method</title>
 </head>
 <body>
    <form action="get_method.php" method="post">
