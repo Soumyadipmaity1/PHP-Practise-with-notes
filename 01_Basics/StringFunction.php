0b1dd47cdccfc3e5e2754bd9076fac32b5895005
smilingsoumya1 <soumyadipmaityjee1@gmail.com>
2024-05-07 22:03:19 +0530

    string updated

--- 01_Basics/StringFunction.php
+++ 01_Basics/StringFunction.php
@@ -6,6 +6,20 @@
     <title>String Function</title>
 </head>
 <body>
-    
+    <?php
+    $username = "Hello World";
+// $username = strlen($username); //length
+// $username = strtoupper($username); //uppercase
+// $username = strtolower($username);   //lowercase
+// $username = str_replace("Hello", "Hi", $username); //replace
+// $username = trim($username); //remove white space
+// $username = str_pad($username, 20, "*"); //padding
+// $username = str_repeat($username, 5); //repeat
+// $username = str_shuffle($username); //shuffle
+// $$username = strrev($username); //reverse    
+// $equal = strcmp("Hello World", $username); //compare
+  $username = substr($username, 6, 5); //substring
+    echo "$username"; 
+    ?>
 </body>
 </html>
